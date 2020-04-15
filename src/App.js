7196ed4062e3c0d887c641bca9fb478922f626b0
Wynn <wynn.blevins@apivista.com>
2020-04-15 13:27:32 -0400

    Lesson 72 complete.  Using styled package.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React, { Component } from 'react';
-import { StyleRoot } from 'radium';
+import styled from 'styled-components';
+
 import './App.css';
 import Person from './Person/Person';
+
+const StyledButton = styled.button`
+  background-color: ${ props => props.alt ? 'red' : 'green' };
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+  &:hover {
+    background-color: ${ props => props.alt ? 'salmon' : 'lightgreen' };
+    color: black;
+  }
+`;
 
 class App extends Component {
   constructor() {
@@ -48,16 +62,7 @@ class App extends Component {
 
   render() {
     const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1px solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightGreen',
-        color: 'black'
-      }
+      
     }
 
     let persons = null;
@@ -71,12 +76,6 @@ class App extends Component {
           })}
         </div>
       )
-
-      style.backgroundColor = 'red';
-      style[':hover'] = {
-        backgroundColor: 'salmon',
-        color: 'black'
-      }
     }
 
     const classes = [];
@@ -90,16 +89,14 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1>Hi, I'm a React App</h1>
-          <p className={classes.join(' ')}>This is really working!</p>
-          <button
-            style={style} 
-            onClick={this.togglePersonHandler}>Switch Name</button>
-          {persons}          
-        </div>  
-      </StyleRoot>
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join( ' ')}>This is really working!</p>
+        <StyledButton alt={this.state.showPersons} onClick={this.togglePersonHandler}>
+          Switch Name
+        </StyledButton>
+        {persons}          
+      </div>  
     );
   }
 }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { StyleRoot } from 'radium';
-import './Person.css';
+import styled from 'styled-components';
+
+const StyledDiv = styled.div`
+    width: 60%;
+    margin: auto;
+    border: 1px solid #eeeeee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    margin-top: 16px;
+    text-align: center;    
+    @media (min-width: 500px) {
+        width: 450px
+    }
+`;
+
 
 const person = ( props ) => {
-    const style = {
-        '@media (min-width: 500px)': {
-            width: '450px'
-        } 
-    };
-    
     return (
-        <StyleRoot>
-            <div className="Person" style={style}>
-                <p>test</p>
-                <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
-                <p>{props.children}</p>
-                <input type="text" onChange={props.changed} value={props.name}></input>
-            </div>
-        </StyleRoot>
+        <StyledDiv>
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name}></input>
+        </StyledDiv>
     )
 };
 
